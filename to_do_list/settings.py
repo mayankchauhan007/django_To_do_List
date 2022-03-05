e8c2fea7d3db936fddf399dc421878175a11f51f
mayankchauhan007 <mayankchauhan20837@gmail.com>
2022-03-06 00:17:11 +0530

    add allowed-host

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d0#ve$x+!gynej5(2g%+8857*dokr))59b0af1_%mlh35sw3vx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["todo-list-python-django.herokuapp.com"]
 
 
 # Application definition
