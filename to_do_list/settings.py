9e1f9a3b51f95df8f1f6f3da158fa9bc3d264831
mayankchauhan007 <mayankchauhan20837@gmail.com>
2022-03-06 18:29:53 +0530

    fixes

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d0#ve$x+!gynej5(2g%+8857*dokr))59b0af1_%mlh35sw3vx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["todo-list-python-django.herokuapp.com","127.0.0.1"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
