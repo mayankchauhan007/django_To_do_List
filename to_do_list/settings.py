1163a3190c144569083cdb9204b808d9cc93b2fa
mayankchauhan007 <mayankchauhan20837@gmail.com>
2022-03-06 18:24:53 +0530

    kuch bhi

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d0#ve$x+!gynej5(2g%+8857*dokr))59b0af1_%mlh35sw3vx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["todo-list-python-django.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,7 +123,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
